19f18a4f76c74bbbb2bf272d0bb1210eeaec1aba
raindelight <62292184+raindelight@users.noreply.github.com>
2025-11-23 18:20:21 +0100

    PR: issue #1 and #25 (#27)
    * refactor: optimize getting solution context, fix bug with incorrect type passing
    
    refs: #1, #25
    
    * fix: profiler output

--- src/main/scala/com/beepboop/app/MainApp.scala
+++ src/main/scala/com/beepboop/app/MainApp.scala
@@ -3,6 +3,7 @@ package com.beepboop.app
 import com.beepboop.app.astar.AStar
 import com.beepboop.app.components.{BinaryExpression, BoolType, ComponentRegistry, Constant, EqualOperator, Expression, Variable}
 import com.beepboop.app.dataprovider.DataProvider
+import com.beepboop.app.logger.Profiler
 import com.beepboop.app.mutations.{AllMutations, ExpressionGenerator, MutationEngine}
 import com.beepboop.parser.{NewMinizincLexer, NewMinizincParser, NewMinizincParserBaseListener}
 import com.typesafe.scalalogging.*
@@ -41,6 +42,9 @@ object MainApp extends LogTrait {
     t.foreach(c => {
       info(c.toString)
     })
+
+    Profiler.report()
+    Profiler.reset()
     /*
     var mutatedExpression = initialExpression
 
