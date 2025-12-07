b3225cb7e84ce32dd444bf1a4a70f94b54e70390
namfohfalo <127258774+namfohfalo@users.noreply.github.com>
2025-12-08 00:29:00 +0100

    issue #40 fixed the csv output of algorithm to contain depth and heuristic (#41)
    Co-authored-by: Olaf Hofman <olaf.hofman@caret.pro>

--- src/main/scala/com/beepboop/app/astar/AStar.scala
+++ src/main/scala/com/beepboop/app/astar/AStar.scala
@@ -65,11 +65,11 @@ class AStar(grammar: ParsedGrammar) extends LogTrait {
   private val mutationEngine = new MutationEngine(AllMutations.mutations)
 
   private val openSet = mutable.PriorityQueue[ModelNodeTMP]()(ModelNodeTMPOrdering)
-  private val visited = mutable.Set[Expression[?]]()
+  private val visited = mutable.Map[Expression[?], ModelNodeTMP]()
   private var isInitialized = false
 
   def getSnapshot: AStarSnapshot = {
-    val visitedNodes = visited.map(expr => ModelNodeTMP(expr, 0, 0)).toSet
+    val visitedNodes = visited.values.toSet
     AStarSnapshot(openSet.toList, visitedNodes)
   }
 
@@ -77,7 +77,9 @@ class AStar(grammar: ParsedGrammar) extends LogTrait {
     this.openSet.clear()
     this.visited.clear()
     snapshot.openSetItems.foreach(item => this.openSet.enqueue(item))
-    this.visited ++= snapshot.visitedItems.map(_.constraint)
+    
+    snapshot.visitedItems.foreach(node => visited(node.constraint) = node)
+
     this.isInitialized = true
     warn(s"State restored! Queue: ${openSet.size}, Visited: ${visited.size}")
   }
@@ -120,7 +122,7 @@ class AStar(grammar: ParsedGrammar) extends LogTrait {
       val currentNode = openSet.dequeue()
       info(s"Dequeuing node: $currentNode")
 
-      visited.add(currentNode.constraint)
+      visited(currentNode.constraint) = currentNode
       debug(visited.toString)
       val generated = generateNeighbors(currentNode)
       info(s"Generated neighbors: ${generated.size}")
@@ -139,7 +141,7 @@ class AStar(grammar: ParsedGrammar) extends LogTrait {
     info(s"Search finished after $iterations iterations.")
     isInitialized = false
     warn(s"Search finished after $iterations iterations without finding a solution.")
-    val resultNodes = visited.map(c => ModelNodeTMP(c, 0, 0))
+    val resultNodes = scala.collection.mutable.Set.from(visited.values)
     Some(resultNodes)
   }
 
@@ -150,7 +152,6 @@ class AStar(grammar: ParsedGrammar) extends LogTrait {
 
       val beta = 2.0
       val betaSq = beta * beta
-
       val SCALING_FACTOR = 1000
 
       val results = (0 until numSolutions).par.map { i =>
