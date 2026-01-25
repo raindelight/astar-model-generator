11bb9f77a779914df13b9ce0437a6bd6fd5abb60
raindelight <dominik.fajfer@gmail.com>
2026-01-25 20:36:00 +0100

    feat: new mutations

--- src/main/scala/com/beepboop/app/mutations/Mutation.scala
+++ src/main/scala/com/beepboop/app/mutations/Mutation.scala
@@ -139,7 +139,119 @@ case class ReplaceSubtree(val maxDepth: Int, enabled: Boolean = true) extends Mu
 
   override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
     val requiredType = expression.signature.output
-    val generated = ExpressionGenerator.generate(requiredType, maxDepth = maxDepth, ctx)
+    val generated = ExpressionGenerator.generate(requiredType, maxDepth = maxDepth, ctx, Some(expression.getClass))
     generated.toList
+  }
+}
+
+case class IteratorCoupling(enabled: Boolean = true) extends Mutation {
+  override def name: String = "IteratorCoupling"
+
+  override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
+    expression match {
+      case ae: ArrayElement[t] =>
+        implicit val tag: ClassTag[t] = ae.ct
+        val localInts = ctx.variables.getOrElse(IntType, Nil)
+
+        localInts.map { varName =>
+          ae.copy(index = Variable[Integer](varName))
+        }
+      case _ => Nil
+    }
+  }
+}
+
+case class UnwrapExpression(enabled: Boolean = true) extends Mutation {
+  override def name: String = "UnwrapExpression"
+
+  override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
+    expression match {
+      case c: ComposableExpression =>
+        val compatibleChildren = c.children.filter(_.signature.output == expression.signature.output)
+
+        compatibleChildren
+      case _ => Nil
+    }
+  }
+}
+
+
+case class PruneToLeaf(enabled: Boolean = true) extends Mutation {
+  override def name: String = "PruneToLeaf"
+
+  override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
+    expression match {
+      case c: ComposableExpression if c.children.nonEmpty =>
+        val requiredType = expression.signature.output
+
+        val simpleReplacements = ComponentRegistry.findCreatablesReturning(requiredType)
+          .filter(_.templateSignature.inputs.isEmpty)
+          .flatMap { factory =>
+            factory match {
+              case v: RandomVariableFactory =>
+                v.availableNames.map(n => v.createWithName(n))
+              case f => Some(f.create(Nil))
+            }
+          }
+
+        scala.util.Random.shuffle(simpleReplacements).take(3).toList
+
+      case _ => Nil
+    }
+  }
+}
+
+
+case class InjectQuantifier(enabled: Boolean = true) extends Mutation {
+  override def name: String = "InjectQuantifier"
+
+  override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
+    if (expression.signature.output != components.BoolType) return Nil
+
+    val factories = List(
+      components.ForAllExpression.ForAllIntListFactory,
+      //components.ExistsExpression.ExistsIntListFactory
+    )
+
+    factories.flatMap { factory =>
+      factory.generateExpression(ctx, (t, c) =>
+        ExpressionGenerator.generate(t, maxDepth = 3, c)
+      )
+    }
+  }
+}
+
+case class InjectNestedLoop(enabled: Boolean = true) extends Mutation {
+  override def name: String = "InjectNestedLoop"
+
+  override def apply(expression: Expression[?], ctx: GenerationContext): List[Expression[?]] = {
+    expression match {
+      case outerLoop: components.ForAllExpression[_] =>
+        val outerIter = outerLoop.iteratorDef.variableName
+        val outerType = outerLoop.iteratorDef.collection.signature.output
+
+        val candidateCollections = DataProvider.variables.filter { v =>
+          v.detailedDataType.isArray &&
+            v.detailedDataType.dataType == "int"
+        }
+
+        if (candidateCollections.isEmpty) return Nil
+
+        val targetCollName = candidateCollections(scala.util.Random.nextInt(candidateCollections.size)).name
+
+        val dependentCollection = components.ArrayElement[List[Integer]](
+          components.Variable[List[List[Integer]]](targetCollName),
+          components.Variable[Integer](outerIter)                   
+        )
+
+        val innerIterName = com.beepboop.app.dataprovider.VarNameGenerator.generateUniqueName("inner")
+        val innerIterator = components.IteratorDef(innerIterName, dependentCollection)
+
+        val newInnerLoop = components.ForAllExpression(innerIterator, outerLoop.body)
+
+        List(components.ForAllExpression(outerLoop.iteratorDef, newInnerLoop))
+
+      case _ => Nil
+    }
   }
 }
